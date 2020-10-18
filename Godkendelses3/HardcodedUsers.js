3c4a997fed8ece8e5e16f2e6cc69d459a1e3a7ef
pelolsen <pelolsen@hotmail.com>
2020-10-18 18:36:05 +0200

    Færdig

--- Godkendelses3/HardcodedUsers.js
+++ Godkendelses3/HardcodedUsers.js
@@ -5,9 +5,9 @@ const InterrestArray =require('./Model/Interest')
 
 
 let userArray = []
-let bob = new PaymentUser ("bob123", "penis", "Bob", "bobiski", "bobobob", "2000-10-10", "male", InterrestArray[0], "4233-029302-123", "06-20", "224");
-let jack = new FreeUser ("jack20", "340302", "Jack", "jackiski", "jack@jack.com", "2000-06-10", "male", InterrestArray[1]);
-let thif = new FreeUser ("thif20", "204903", "Thifanny", "Thifiski", "thif@jack.com", "2000-09-12", "famale", InterrestArray[0]);
+let bob = new PaymentUser ("bob123", "23342km", "Bob", "bobiski", "bobobob@bobtheman.dk", "2000-10-10", "male", InterrestArray[0], "4233-029302-123", "06-20", "224");
+let jack = new FreeUser ("jack20", "340302se", "Jack", "jackiski", "jack@jack.com", "2000-06-10", "male", InterrestArray[1]);
+let thif = new FreeUser ("thif20", "204903er", "Thifanny", "Thifiski", "thif@jack.com", "2000-09-12", "famale", InterrestArray[0]);
 
 userArray = [bob, jack, thif]
 
